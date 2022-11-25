ab8536d7e52077d502d4c6d06d56e38a2363b764
Dave Hale <davejlhale@yahoo.co.uk>
2022-11-25 14:01:21 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,171 @@
-import logo from './logo.svg';
+
 import './App.css';
+import buttons from './components/buttonArray'
+import parse from 'html-react-parser'
+import { useState } from 'react';
+import { evaluate,match, sqrt, log10, format } from 'mathjs';
+
+import { square } from 'mathjs';
+
 
 function App() {
+  const [answer, setAnswer] = useState("0");
+  const [runningSum, setRunningSum] = useState("");
+  const [lastFunc, setLastFunc] = useState("");
+  const parseInput = (funcs) => {
+
+    let t = 0;
+    let lastnum = 0;
+    switch (funcs) {
+      case "AC":
+        setAnswer(0);
+        setRunningSum("");
+        break;
+      case "=":
+        lastnum = runningSum.replace("log","log10");
+        lastnum = lastnum.replace("ln","log");
+        // setRunningSum(lastnum);
+        try {
+          setAnswer(evaluate(lastnum))
+        } catch (err) {
+          console.log("e", err)
+        }
+        break;
+      case "ANS":
+        let ans=answer.toString();
+        console.log(ans)
+        setRunningSum(ans);
+        break;
+      case "(-)":
+        
+        let running =  runningSum;
+        
+        console.log(running,runningSum,)
+        lastnum = running.match(/[\d\.]+(?!.[\.*\d])/);
+        console.log("ln",lastnum)
+        running=running.replace((/[\d\.]+(?!.*[\.\d])/), (-1*lastnum))
+        running=running.replace('--','');
+        setRunningSum(running)
+        break;
+      case "sin":
+        funcs = "sin(";
+        setRunningSum(runningSum + funcs);
+        break;
+      case "hyp":
+        funcs = "hyp(";
+        setRunningSum(runningSum + funcs);
+        break;
+      case "cos":
+        funcs = "cos(";
+        setRunningSum(runningSum + funcs);
+        break;
+      case "tan":
+        funcs = "tan(";
+        setRunningSum(runningSum + funcs);
+        break;
+      case "log":
+        funcs = "log(";
+        setRunningSum(runningSum + funcs);
+        // lastnum = runningSum.match(/[\d\.]+(?!.[\.*\d])/)[0];
+        // setRunningSum(runningSum.replace((/[\d\.]+(?!.*[\.\d])/), log10(lastnum)))
+        break;
+        case "ln":
+          funcs = "ln(";
+          setRunningSum(runningSum + funcs);
+          break;
+      case "&#x221A;":
+        lastnum = runningSum.match(/[\d\.]+(?!.[\.*\d])/)[0];
+        console.log(lastnum)
+
+        setRunningSum(runningSum.replace((/[\d\.]+(?!.*[\.\d])/), sqrt(lastnum)))
+        break;
+      case "x<sup>2</sup>":
+        lastnum = runningSum.match(/[\d\.]+(?!.[\.*\d])/)[0];
+        setRunningSum(runningSum.replace((/[\d\.]+(?!.*[\.\d])/), square(lastnum)))
+        break;
+
+      case "x": funcs = "*"
+      case ".":
+      case "(":
+      case ")":
+      case "+":
+      case "-":
+      case "/":
+      case "1":
+      case "2":
+      case "3":
+      case "4":
+      case "5":
+      case "6":
+      case "7":
+      case "8":
+      case "9":
+      case "0": setRunningSum(runningSum + funcs);
+    }
+
+
+
+
+
+
+
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator">casio
+      <Display runningSum={runningSum} answer={answer} />
+      <Buttons buttons={buttons} click={parseInput}></Buttons>
     </div>
   );
 }
 
+
+
+
+const Display = (props) => {
+  return (
+
+    <div id="display">
+      <RunningSum runningSum={props.runningSum} />
+      <Answer answer={props.answer} />
+    </div>
+  )
+}
+
+const Buttons = (props) => {
+  let btns = props.buttons.sort((a, b) => parseFloat(a.keyOrder) - parseFloat(b.keyOrder));
+  return (
+    <div className="buttons">
+      {btns.map((text, index) => {
+
+        return <Button key={index} button={btns[index]} click={props.click} />
+
+      })}
+    </div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <div id={props.button.keyStyle} onClick={() => { props.click(props.button.text) }}>
+      {parse(props.button.text)}
+    </div>
+  )
+}
+const RunningSum = (props) => {
+  return (
+    <div className="running-sum">
+      <p>{props.runningSum}</p>
+    </div>
+  )
+}
+
+const Answer = (props) => {
+  return (
+    <div className="answer">
+      <p>{props.answer}</p>
+    </div>
+  )
+}
 export default App;
---
+++ src/components/buttonArray.js
@@ -0,0 +1,304 @@
+const buttons = [
+    {
+        text: "CALC", //to display on html
+        function: "",
+        image: "", //src
+        keyOrder: "1",
+        keyStyle: "small"
+    },
+    {
+        text: "dx", //to display on html
+        function: "",
+        image: "", //src
+        keyOrder: "1",
+        keyStyle: "small"
+    },
+    {
+        text: "", //to display on html
+        function: "",
+        image: "", //src
+        keyOrder: "1",
+        keyStyle: "missing"
+    },
+    {
+        text: "", //to display on html
+        function: "",
+        image: "", //src
+        keyOrder: "1",
+        keyStyle: "missing"
+    },
+    {
+        text: "x<sup>-1</sup>", //to display on html
+        function: "",
+        image: "", //src
+        keyOrder: "1",
+        keyStyle: "small"
+    },
+    {
+        text: "const", //to display on html
+        function: "",
+        image: "", //src
+        keyOrder: "1",
+        keyStyle: "small"
+    },
+    {
+        text: "a<sup>b/<sup><sub>c</sub>", //to display on html
+        function: "frd",
+        image: "", //src
+        keyOrder: "1",
+        keyStyle: "small"
+    },
+    //&#x221A;
+    {
+        text: "&#x221A;", //to display on html
+        function: "",
+        image: "", //src
+        keyOrder: "1",
+        keyStyle: "small"
+    },
+    {
+        text: "x<sup>2</sup>", //to display on html
+        function: "",
+        image: "", //src
+        keyOrder: "1",
+        keyStyle: "small"
+    },
+    {
+        text: "^", //to display on html
+        function: "",
+        image: "", //src
+        keyOrder: "1",
+        keyStyle: "small"
+    },
+    {
+        text: "log", //to display on html
+        function: "",
+        image: "", //src
+        keyOrder: "1",
+        keyStyle: "small"
+    },
+    {
+        text: "ln", //to display on html
+        function: "",
+        image: "", //src
+        keyOrder: "1",
+        keyStyle: "small"
+    },
+    {
+        text: "(-)", //to display on html
+        function: "",
+        image: "", //src
+        keyOrder: "1",
+        keyStyle: "small"
+    },
+    {
+        text: "&#9675;'''", //to display on html
+        function: "",
+        image: "", //src
+        keyOrder: "1",
+        keyStyle: "small"
+    },
+    {
+        text: "hyp", //to display on html
+        function: "",
+        image: "", //src
+        keyOrder: "1",
+        keyStyle: "small"
+    },
+    {
+        text: "sin", //to display on html
+        function: "",
+        image: "", //src
+        keyOrder: "1",
+        keyStyle: "small"
+    },
+
+    {
+        text: "cos", //to display on html
+        function: "",
+        image: "", //src
+        keyOrder: "1",
+        keyStyle: "small"
+    },
+
+    {
+        text: "tan", //to display on html
+        function: "",
+        image: "", //src
+        keyOrder: "1",
+        keyStyle: "small"
+    },
+    {
+        text: "RCL", //to display on html
+        function: "",
+        image: "", //src
+        keyOrder: "1",
+        keyStyle: "small"
+    },
+    {
+        text: "ENG", //to display on html
+        function: "",
+        image: "", //src
+        keyOrder: "1",
+        keyStyle: "small"
+    },
+    {
+        text: "(", //to display on html
+        function: "",
+        image: "", //src
+        keyOrder: "1",
+        keyStyle: "small"
+    },
+    {
+        text: ")", //to display on html
+        function: "",
+        image: "", //src
+        keyOrder: "1",
+        keyStyle: "small"
+    },
+    {
+        text: "'", //to display on html
+        function: "",
+        image: "", //src
+        keyOrder: "1",
+        keyStyle: "small"
+    },
+    {
+        text: "M+", //to display on html
+        function: "",
+        image: "", //src
+        keyOrder: "1",
+        keyStyle: "small"
+    },
+
+
+    {
+        text: "7", //to display on html
+        function: "",
+        image: "", //src
+        keyOrder: "1",
+        keyStyle: "normal"
+    }, {
+        text: "8", //to display on html
+        function: "",
+        image: "", //src
+        keyOrder: "1",
+        keyStyle: "normal",
+    }, {
+        text: "9", //to display on html
+        function: "",
+        image: "", //src
+        keyOrder: "1",
+        keyStyle: "normal"
+    }, 
+    {
+        text: "DEL", //to display on html
+        function: "",
+        image: "", //src
+        keyOrder: "1",
+        keyStyle: "normal-red"
+    },
+    {
+        text: "AC", //to display on html
+        function: "",
+        image: "", //src
+        keyOrder: "1",
+        keyStyle: "normal-red"
+    },
+    
+    {
+        text: "4", //to display on html
+        function: "",
+        image: "", //src
+        keyOrder: "1",
+        keyStyle: "normal"
+    }, {
+        text: "5", //to display on html
+        function: "",
+        image: "", //src
+        keyOrder: "1",
+        keyStyle: "normal"
+    }, {
+        text: "6", //to display on html
+        function: "",
+        image: "", //src
+        keyOrder: "1",
+        keyStyle: "normal"
+    }, {
+        text: "x", //to display on html
+        function: "",
+        image: "", //src
+        keyOrder: "1",
+        keyStyle: "normal"
+    }, {
+        text: "/", //to display on html
+        function: "",
+        image: "", //src
+        keyOrder: "1",
+        keyStyle: "normal"
+    }, {
+        text: "1", //to display on html
+        function: 1,
+        image: "", //src
+        keyOrder: "1",
+        keyStyle: "normal"
+    }, {
+        text: "2", //to display on html
+        function: "",
+        image: "", //src
+        keyOrder: "1",
+        keyStyle: "normal"
+    }, {
+        text: "3", //to display on html
+        function: "",
+        image: "", //src
+        keyOrder: "1",
+        keyStyle: "normal"
+    }, {
+        text: "+", //to display on html
+        function: "",
+        image: "", //src
+        keyOrder: "1",
+        keyStyle: "normal"
+    }, {
+        text: "-", //to display on html
+        function: "",
+        image: "", //src
+        keyOrder: "1",
+        keyStyle: "normal"
+    }, {
+        text: "0", //to display on html
+        function: "",
+        image: "", //src
+        keyOrder: "1",
+        keyStyle: "normal"
+    },{
+        text: ".", //to display on html
+        function: "",
+        image: "", //src
+        keyOrder: "1",
+        keyStyle: "normal"
+    },{
+        text: "EXP", //to display on html
+        function: "",
+        image: "", //src
+        keyOrder: "1",
+        keyStyle: "normal"
+    },
+    {
+        text: "ANS", //to display on html
+        function: "",
+        image: "", //src
+        keyOrder: "1",
+        keyStyle: "normal"
+    },  {
+        text: "=", //to display on html
+        function: "",
+        image: "", //src
+        keyOrder: "1",
+        keyStyle: "normal"
+    }
+
+]
+
+export default buttons;
